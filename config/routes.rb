28bb8e80d6445883a769cc490e30440504649119
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-16 12:44:14 +0300

    Add user_id to the roots for uniqueness

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root "users#index"
   get "/users" , to: "users#index",  as: "users"
   get "/users/:id" , to: "users#show" , as: 'user'
-  get "/users/:id/posts" , to: "posts#index", as: "user_posts"
-  get "/users/:id/posts/:id" , to: "posts#show", as: "user_post"
+  get "/users/:user_id/posts" , to: "posts#index", as: "user_posts"
+  get "/users/:user_id/posts/:id" , to: "posts#show", as: "user_post" 
 end
 
