c78aec9e5a35559d60be8040a2464682fa9f5349
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-24 13:32:06 +0300

    Add modified requests files for tests to pass

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
-describe 'get users from /users' do
-  it 'returns a successful response' do
-    get '/users'
+describe 'Get user details' do
+  let(:user) { User.create!(name: 'Test User', bio: 'Test bio', posts_counter: 0) }
 
-    expect(response).to be_successful
-    expect(response.body).to include('<h1>This is a list of posts from one user!</h1>')
-    expect(response).to render_template(:index)
-  end
-end
-
-describe 'get users from /users/:id' do
-  it 'returns a successful response' do
-    user = User.create!(name: 'Test User No.1', posts_counter: 0)
+  it 'returns a successful response and renders user details' do
     get "/users/#{user.id}"
 
     expect(response).to be_successful
-    expect(response.body).to include('<h2>This is one post from user!</h2>')
     expect(response).to render_template(:show)
+
+    expect(response.body).to include(user.bio)
+
+    user.recent_posts.each do |post|
+      expect(response.body).to include(post.title)
+      expect(response.body).to include(post.text)
+    end
   end
 end
