ca63f133fb3809c0e31915c43835753829a50006
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-25 14:51:59 +0300

    Add modifications to the likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource :user
+  load_and_authorize_resource :post, through: :user
+
   def new
-    @user = User.find(params[:user_id])
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.new
-    @like.author = @user
+    @like = @post.likes.build(author: @user)
+    
     if @like.save
       redirect_to user_post_path(@user, @post)
     else
