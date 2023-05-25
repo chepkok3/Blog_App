4ea233cd80acff2a3a8ac42c30019609449036cd
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-25 20:06:14 +0700

    Add authorization of user on comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.author = @user
+    if @comment.save
+      redirect_to request.referrer
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referrer
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
