41a9989f389d8bd156464288bed407c16f628a99
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-05-25 20:04:21 +0700

    Add authorization of user on posts

---
+++ app/controllers/posts.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+  load_and_authorize_resource
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments)
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_posts_path(@user)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
