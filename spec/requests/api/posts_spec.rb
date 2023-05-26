fcce09c1e1ad2d9ef9a1614bbf678b632e2aa270
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-05-26 11:56:24 +0300

    Add linter error fix

--- spec/requests/api/posts_spec.rb
+++ spec/requests/api/posts_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe 'api/posts', type: :request do
 
       response '200', 'posts found' do
         schema type: :object,
-          properties: {
-            id: { type: :integer },
-            title: { type: :string },
-            text: { type: :string },
-            created_at: { type: :string },
-            updated_at: { type: :string }
-          },
-          required: [ 'id', 'title', 'text', 'created_at', 'updated_at' ]
+               properties: {
+                 id: { type: :integer },
+                 title: { type: :string },
+                 text: { type: :string },
+                 created_at: { type: :string },
+                 updated_at: { type: :string }
+               },
+               required: %w[id title text created_at updated_at]
 
         run_test!
       end
